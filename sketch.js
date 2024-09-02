4deee5bc428636a51d1af2e3ffcc6bab138bb183
hannahmass <hannahmassey@ucsb.edu>
2024-09-02 15:18:37 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,22 +3,20 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(155, 147, 50);
  
-  for (let x = 0; x < width; x += 5) {
-    stroke('blue')
-    strokeWeight(2)
-    line(x, 0, x, 400);
+ let x=sin(frameCount * 0.09) * 100 + width/2;
+  circle (x, height/2, 50); 
+  fill('pink'); 
+  
+  let g=sin(frameCount * 0.1) *100 + width/2;
+  {
+    circle(g,height/2, 50);
+    fill('orange')}
+  let w=sin (frameCount * 0.08) *100 +width/2; 
+  {circle (w, height/2, 50)
+   fill('purple')
   }
-  for (let x = 0; x < width; x += 6) {
-    line(x, 0, x, 400);
-    stroke('red')
-    strokeWeight(3)
   }
-  for (let y = 0; y < width; y += 10) {
-    line(0,y,400, y)
-    stroke('green')
-    strokeWeight(4)
- 
-  }
-}
+  
+  
